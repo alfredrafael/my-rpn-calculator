2fbebcac7b9382c425bd33fb94b4076c75078269
alfredrafael <alfredrafael@gmail.com>
2021-11-19 11:22:03 -0600

    finishes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Box,
   AppBar,
@@ -13,8 +13,11 @@ import {
   Typography,
   Button,
   ButtonGroup,
+  TextField,
+  Input,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import "./app.css";
 
 const useStyles = makeStyles((theme) => ({
   myLogo: {
@@ -33,88 +36,70 @@ const useStyles = makeStyles((theme) => ({
     textAlign: "center",
   },
   calcButton: {
-    width: "3.5em",
+    width: "4em",
   },
-  calcBtnDouble: {
-    width: "7em",
+  activeBtn: {
+    background: "#040182",
+    color: "white",
+    width: "4em",
   },
-  btnRow: {
-    marginBottom: "1%",
-  },
-  myBtnRow: {
-    margin: "2% 0",
+  resultBtn: {
+    width: "fit-content",
   },
 }));
+
+const allOperations = [
+  {
+    name: "plus",
+    sign: "+",
+    className: "",
+  },
+  {
+    name: "minus",
+    sign: "-",
+    className: "",
+  },
+  {
+    name: "division",
+    sign: "/",
+    className: "",
+  },
+  {
+    name: "multiplication",
+    sign: "*",
+    className: "",
+  },
+];
 
 //////////////////////////////////////////////////////
 
 /////////////////////////////////////////////////////
 
 const App = () => {
-  const [calc, setCalc] = useState("");
-  const [result, setResult] = useState("");
-  const [stack, setStack] = useState([]);
+  const [valueA, setValueA] = useState();
+  const [valueB, setValueB] = useState();
+  const [operations, setOperations] = useState();
+  const [result, setResult] = useState(0);
 
-  const ops = ["/", "*", "+", "-", "."];
-
-  const enter = (value) => {
-    const innerStack = [];
-    setStack(value);
-    return innerStack;
-  };
-
-  const updateCalc = (value) => {
-    if (
-      (ops.includes(value) && calc === "") ||
-      (ops.includes(value) && ops.includes(calc.slice(-1)))
-    ) {
-      return;
+  const handleSubmit = () => {
+    if (operations === "+") {
+      setResult(parseInt(valueA) + parseInt(valueB));
     }
-    setCalc(calc + value);
-
-    if (!ops.includes(value)) {
-      setResult(eval(calc + value).toString());
+    if (operations === "-") {
+      setResult(parseInt(valueA) - parseInt(valueB));
+    }
+    if (operations === "*") {
+      setResult(parseInt(valueA) * parseInt(valueB));
+    }
+    if (operations === "/") {
+      setResult(parseInt(valueA) / parseInt(valueB));
     }
   };
 
-  const calculate = () => {
-    setCalc(eval(calc).toString());
-  };
-
-  const deleteLast = () => {
-    if (calc === "") {
-      return;
-    }
-    const value = calc.slice(0, -1);
-    setCalc(value);
-  };
-
-  const clearAll = () => {
-    if (calc === "") {
-      return;
-    }
-    const value = "";
-    const result = "";
-    setCalc(value);
-    setResult(result);
-  };
-
-  const createDigits = () => {
-    const digits = [];
-
-    for (let i = 1; i < 10; i++) {
-      digits.push(
-        <Button
-          onClick={() => updateCalc(i.toString())}
-          key={i}
-          className={myClasses.calcButton}
-        >
-          {i}
-        </Button>
-      );
-    }
-
-    return digits;
+  const clearField = () => {
+    setValueA(0);
+    setValueB(0);
+    setResult(0);
   };
 
   const myClasses = useStyles();
@@ -145,74 +130,66 @@ const App = () => {
               >
                 RPM Calculator
               </Typography>
-              <div
-                style={{
-                  textAlign: "left",
-                  marginLeft: "20%",
-                  marginBottom: "2%",
-                }}
-              >
-                {enter ? <span>({enter})</span> : "ddd"}
-              </div>
-
               <Grid container spacing={2} justify="center">
-                <div className={myClasses.display}>{calc || "0"}</div>
-              </Grid>
-              <Grid container spacing={2} justify="center">
-                <div style={{}}>
+                <div style={{ display: "flex", flexDirection: "column" }}>
+                  <Input
+                    text
+                    placeholder="Enter Value A"
+                    value={valueA}
+                    onChange={(e) => setValueA(e.target.value)}
+                    inputProps={{ "aria-label": "description" }}
+                  />
+                  <br />
+                  <Input
+                    placeholder="Enter Value B"
+                    value={valueB}
+                    onChange={(e) => setValueB(e.target.value)}
+                    inputProps={{ "aria-label": "description" }}
+                  />
+                  <br />
+                  Choose Operation
+                  <br />
+                  <br />
                   <ButtonGroup
                     variant="contained"
                     aria-label="outlined primary button group"
-                    className={myClasses.myBtnRow}
+                    className={myClasses.btnRow}
                   >
-                    {createDigits()}
-                  </ButtonGroup>
-                </div>
-              </Grid>
-              <Grid container spacing={2} justify="center">
-                <ButtonGroup
-                  variant="contained"
-                  aria-label="outlined primary button group"
-                  className={myClasses.btnRow}
-                >
-                  <Button onClick={clearAll} className={myClasses.calcButton}>
-                    C
-                  </Button>
+                    <Button
+                      onClick={clearField}
+                      className={myClasses.calcButton}
+                    >
+                      CLEAR
+                    </Button>
 
+                    {allOperations.map((operation, i) => (
+                      <Button
+                        onClick={() => setOperations(operation.sign)}
+                        className={
+                          operations == operation.sign
+                            ? myClasses.activeBtn
+                            : myClasses.calcButton
+                        }
+                        id={operation.name}
+                      >
+                        {operation.sign}
+                      </Button>
+                    ))}
+                  </ButtonGroup>
+                  <br />
                   <Button
-                    onClick={() => updateCalc("+")}
-                    className={myClasses.calcButton}
+                    variant="contained"
+                    onClick={handleSubmit}
+                    className={myClasses.resultBtn}
                   >
-                    +
+                    Get Result
                   </Button>
-                  <Button
-                    onClick={() => updateCalc("-")}
-                    className={myClasses.calcButton}
-                  >
-                    -
-                  </Button>
-                  <Button
-                    onClick={() => updateCalc("*")}
-                    className={myClasses.calcButton}
-                  >
-                    *
-                  </Button>
-                  <Button
-                    onClick={() => updateCalc("/")}
-                    className={myClasses.calcButton}
-                  >
-                    /
-                  </Button>
-                  <Button onClick={calculate} className={myClasses.calcButton}>
-                    =
-                  </Button>
-                  <Button
-                    onClick={() => enter()}
-                    className={myClasses.calcButton}
-                  >
-                    ENTER
-                  </Button>
-                </ButtonGroup>
+                  <br />
+                  <Input
+                    value={result}
+                    inputProps={{ "aria-label": "description" }}
+                  />
+                </div>
               </Grid>
             </Container>
           </div>
